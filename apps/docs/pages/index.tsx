577f393d900dade87bdf16bed9fee6d412eea5b1
fandyajpo <fandyglitch3@gmail.com>
2023-04-17 09:31:39 +0800

    dads

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Input } from "ui";
+import { Button } from "ui";
 import Pagar from "../components/Page";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 import { useCtx } from "../lib/Context";
@@ -21,14 +21,14 @@ export default function Docs() {
     <>
       <Button />
       <Pagar />
-      <form onSubmit={handleSubmit(onSubmit)}>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
         <Controller
           render={({ field }) => <Input {...field} />}
           {...register}
           name="name"
           control={control}
         />
-      </form>
+      </form> */}
     </>
   );
 }
--- packages/ui/Input.tsx
+++ packages/ui/Input.tsx
@@ -2,7 +2,7 @@ import { forwardRef, HTMLProps } from "react";
 
 const Input = forwardRef<HTMLInputElement, HTMLProps<HTMLInputElement>>(
   (props, ref) => {
-    return <input className="border border-gray-300" {...props} ref={ref} />;
+    return <input className="border border-gray-300" />;
   }
 );
 
